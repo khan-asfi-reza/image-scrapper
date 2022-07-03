2c20e016347ac000f968a539f0cfbca2c4281228
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-03 22:16:54 +0600

    :art: Improve Structure of the Code [Black]

--- scrapper/core/urls.py
+++ scrapper/core/urls.py
@@ -1,17 +1,29 @@
 from django.urls import path
 
 from scrapper.core.apis import (
+    ImageDetailsAPI,
+    ImageListAPI,
+    ImageOriginalURLQueryAPI,
     URLImageScrappingAPI,
     URLImagesDeleteScrapeAPI,
-    ImageListAPI,
-    ImageDetailsAPI,
-    ImageOriginalURLQueryAPI
 )
 
 urlpatterns = [
     path("url/", URLImageScrappingAPI.as_view(), name="url-view"),
     path("images/list/", ImageListAPI.as_view(), name="image-list-view"),
-    path("images/restore/", URLImagesDeleteScrapeAPI.as_view(), name="image-restore-view"),
-    path("images/details/<int:id>", ImageDetailsAPI.as_view(), name="image-detail-view"),
-    path("images/query/", ImageOriginalURLQueryAPI.as_view(), name="image-query-view"),
+    path(
+        "images/restore/",
+        URLImagesDeleteScrapeAPI.as_view(),
+        name="image-restore-view",
+    ),
+    path(
+        "images/details/<int:id>",
+        ImageDetailsAPI.as_view(),
+        name="image-detail-view",
+    ),
+    path(
+        "images/query/",
+        ImageOriginalURLQueryAPI.as_view(),
+        name="image-query-view",
+    ),
 ]
